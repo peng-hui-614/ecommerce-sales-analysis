97fa5984af8a9ab1b9e4bdbcc3cb209492497693
peng-hui-614 <2430163331@qq.com>
2025-11-20 14:23:26 +0800

    Initialize tasks module with imports and exports

---
+++ src/tasks/__init__.py
@@ -0,0 +1,6 @@
+from .task1_preprocessing import Task1Preprocessor
+from .task2_multidimensional import Task2Analyzer
+from .task3_forecasting import Task3Forecaster
+from .task4_optimization import Task4Optimizer
+
+__all__ = ['Task1Preprocessor', 'Task2Analyzer', 'Task3Forecaster', 'Task4Optimizer']
